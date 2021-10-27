7e9037d166cab084c44b1aaf1c36c74f62f13de2
fadhilahm <fadhilahmetra@gmail.com>
2021-10-27 21:36:29 +0700

    Created template vue app and revived the library project from the dead.

---
+++ test-app/src/views/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Hello I'm consuming the component library</h1>
+    <sample-component
+      heading-text="This is a text component"
+      body-text="Made with love by Fadhilahm"
+    />
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from "vue-property-decorator";
+
+import { SampleComponent } from "vue-component-library-harvey";
+
+@Component({
+  SampleComponent,
+})
+export default class Test extends Vue {}
+</script>
+
+<style></style>
---
+++ vue-component-library-harvey/README.md
@@ -0,0 +1,2 @@
+# vue-component-library-harvey
+Test repo for trying out making my own Vue component library.
